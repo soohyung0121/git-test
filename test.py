647bd7ccd4fc529c497077bd18faab941358c287
김수형 <soohyung0121@gmail.com>
2020-07-16 00:05:07 +0900

    first change

--- test.py
+++ test.py
@@ -4,9 +4,5 @@ def odd_number(number):
     number = random.randrange(1, 100)
     if number & 2 == 0:
         print("2의 배수.")
-    elif number & 3 == 0:
-        print("3의 배수")
-    elif number & 5 == 0:
-        print("5의 배수")
     else:
         print("숫자입니다.")
