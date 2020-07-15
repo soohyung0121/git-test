34ab31d338b5e6150ef55c278742d1319caa6986
김수형 <soohyung0121@gmail.com>
2020-07-16 00:57:40 +0900

    1111

--- test.py
+++ test.py
@@ -6,7 +6,5 @@ def odd_number(number):
         print("2의 배수.")
     elif number & 3 == 0:
         print("3의 배수")
-    elif number & 5 == 0:
-        print("5의 배수")
     else:
         print("그냥 숫자입니다.")
