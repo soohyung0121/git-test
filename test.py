c2fe43f1ed0f35558e24fb528f66acd3253f4852
김수형 <soohyung0121@gmail.com>
2020-07-15 23:28:29 +0900

    real merge

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
+import random
+
 def odd_number(number):
+    number = random.randrange(1, 100)
     if number & 2 == 0:
-        print("짝수입니다.")
+        print("2의 배수.")
+    elif number & 3 == 0:
+        print("3의 배수")
+    elif number & 5 == 0:
+        print("5의 배수")
+    elif number & 7 == 0:
+        print("7의 배수")
+    elif number & 9 == 0:
+        print("그냥 넣어봤습니다.")
     else:
-        print("홀수입니다.")
+        print("숫자입니다.")
